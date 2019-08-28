ed5577880c0f266054d27269ca948f4edc9b1bf5
Sanket Bhalerao <ketu.bhalerao@gmail.com>
2019-08-29 00:06:18 +0530

    e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Tour OF Heros');
+    expect(page.getTitleText()).toEqual('Tour Of Heros');
   });
 
   afterEach(async () => {
